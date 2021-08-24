37f4c7bc4568f43ce3e1bdbdbd873becee7bfb77
Yvan-BM <yvan.t.biakeu@aims-senegal.org>
2021-08-24 02:16:28 +0000

    new commit

--- src/Transformer.py
+++ src/Transformer.py
@@ -1,20 +1,10 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from Transformers.Layers import EncoderLayer, DecoderLayer
+from src.Layers import EncoderLayer, DecoderLayer
+from src.utils import get_pad_mask, get_subsequent_mask
 
 '''_author = Yvan Tamdjo'''
-
-def get_pad_mask(seq, pad_idx):
-    return (seq != pad_idx).unsqueeze(-2)
-
-
-def get_subsequent_mask(seq):
-    ''' For masking out the subsequent info. '''
-    sz_b, len_s = seq.size()
-    subsequent_mask = (1 - torch.triu(
-    torch.ones((1, len_s, len_s), device=seq.device), diagonal=1)).bool()
-    return subsequent_mask
 
 class Encoder(nn.Module):
     "Core encoder is a stack of N layers"
